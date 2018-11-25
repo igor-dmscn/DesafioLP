756e95d7924d678681892516717f2ec2e95ab59d
Gdiael Barros <gdiael.barros@gmail.com>
2018-11-25 07:31:31 -0300

    Add funções arquivos e pequenos ajustes

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(){
 
 	a.insereContato("Igor", "Igor", "84 99999-0000", "84 99999-0000", "igor@gmail.com");
 	a.insereContato("Gdiael", "Gdi", "84 98805-4083", "84 98805-4083", "gdiael.barros@gmail.com");
-	a.insereContato("Manoela","Manu", "84 99930-0303", "84 99930-0303", "manuela.souza@gmail.com");
+	a.insereContato("Manuela","Manu", "84 99930-0303", "84 99930-0303", "manuela.souza@gmail.com");
 
 	a.listaContato();
 
